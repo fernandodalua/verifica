574dea0b78071a1efcf4e6f1187e6d0794cd10a6
Junior <junior@7bi.me>
2017-05-04 23:11:41 -0300

    EditName fixed

--- client/lib/events/profile_form.js
+++ client/lib/events/profile_form.js
@@ -2,7 +2,8 @@ Template.profileForm.events({
 'submit form': function(e, template) {
 e.preventDefault();
 var inputs = template.findAll('input');
-Meteor.users.update({_id: this._id},{$set: {'profile.name': inputs[0].value}});
+console.log("this: ",this,"\ninput: ",inputs[0].value);
+Meteor.users.update({_id: this.user._id,},{$set: {"profile.name": inputs[0].value}});
 Session.set("editProfile", false);
 }
 });
--- client/lib/routes.js
+++ client/lib/routes.js
@@ -1,24 +1,26 @@
 Router.map(function() {
-this.route('home', {
-path: '/',
-template: 'home',
-layoutTemplate: 'layout',
-data: function() {
-return {
-posts: Post.list(Meteor.userId())
-}
-},
-});
-this.route('user', {
-path: '/user/:_id',
-template: 'user',
-layoutTemplate: 'layout',
-data: function() {
-var _id = this.params._id;
-return {
-user: Meteor.users.findOne({_id: _id}),
-posts: Post.list(_id)
-}
-},
-});
+  this.route('home', {
+    path: '/',
+    template: 'home',
+    layoutTemplate: 'layout',
+    data: function() {
+      return {
+        posts: Post.list(Meteor.userId()),
+        user: Meteor.users.findOne({_id:Meteor.userId()})
+
+      }
+    },
+  });
+  this.route('user', {
+    path: '/user/:_id',
+    template: 'user',
+    layoutTemplate: 'layout',
+    data: function() {
+      var _id = this.params._id;
+      return {
+        user: Meteor.users.findOne({_id: _id}),
+        posts: Post.list(_id)
+      }
+    },
+  });
 });
